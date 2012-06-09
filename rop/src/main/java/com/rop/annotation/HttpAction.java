43d87cbe6d3514a6ea78a0f6e4aa480f0a5e46e9
stamen <quickselect@yahoo.com.cn>
2012-06-09 17:07:39 +0800

    原来GIT提交有乱码，该注释用于测试。

---
+++ rop/src/main/java/com/rop/annotation/HttpAction.java
@@ -0,0 +1,29 @@
+/**
+ * 版权声明：中图一购网络科技有限公司 版权所有 违者必究 2012 
+ * 日    期：12-6-8
+ */
+package com.rop.annotation;
+
+/**
+ * <pre>
+ *   请求类型的方法
+ * </pre>
+ *
+ * @author 陈雄华
+ * @version 1.0
+ */
+public enum HttpAction {
+
+    GET,POST;
+    
+    public static HttpAction fromValue(String value){
+        if(GET.name().equalsIgnoreCase(value)){
+            return GET;
+        }else if(POST.name().equalsIgnoreCase(value)){
+            return POST;
+        }else{
+            return POST;
+        }
+    }
+}
+
