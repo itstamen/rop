c1fd01c552a67bd18c8d6099ced851b2e3214564
stamen <quickselect@yahoo.com.cn>
2012-08-27 16:03:03 +0800

    1.add id property to the <rop:annotation-driver/>

--- rop-sample/src/test/java/com/rop/config/RopNamespaceHandlerIT.java
+++ rop-sample/src/test/java/com/rop/config/RopNamespaceHandlerIT.java
@@ -10,9 +10,14 @@ import com.rop.sample.SampleAppSecretManager;
 import com.rop.sample.SampleServiceAccessController;
 import com.rop.sample.request.Telephone;
 import com.rop.security.DefaultSecurityManager;
+import org.springframework.context.ApplicationContext;
 import org.testng.annotations.Test;
 import org.unitils.UnitilsTestNG;
+import org.unitils.database.annotations.Transactional;
+import org.unitils.database.util.TransactionMode;
 import org.unitils.spring.annotation.SpringApplicationContext;
+import org.unitils.spring.annotation.SpringBean;
+import org.unitils.spring.annotation.SpringBeanByName;
 import org.unitils.spring.annotation.SpringBeanByType;
 
 import java.util.concurrent.TimeUnit;
@@ -28,8 +33,12 @@ import static org.testng.Assert.*;
  * @version 1.0
  */
 
-//@Transactional(TransactionMode.DISABLED)
+@Transactional(TransactionMode.DISABLED)
+@SpringApplicationContext("com/rop/config/simplestRopConfig.xml")
 public class RopNamespaceHandlerIT extends UnitilsTestNG {
+
+    @SpringBean("router")
+    AnnotationServletServiceRouter serviceRouter;
 
     /**
      * 最简单的配置
@@ -37,9 +46,7 @@ public class RopNamespaceHandlerIT extends UnitilsTestNG {
      * @param serviceRouter
      */
     @Test
-    @SpringApplicationContext("com/rop/config/simplestRopConfig.xml")
-    @SpringBeanByType
-    public void testSimplestConfig(AnnotationServletServiceRouter serviceRouter) {
+    public void testSimplestConfig() {
         assertNotNull(serviceRouter);
         RopContext ropContext = serviceRouter.getRopContext();
         assertNotNull(ropContext);
@@ -54,10 +61,11 @@ public class RopNamespaceHandlerIT extends UnitilsTestNG {
         assertNotNull(serviceRouter.getSecurityManager());
         assertNotNull(serviceRouter.getRopEventMulticaster());
         assertNotNull(serviceRouter.getThreadPoolExecutor());
-        assertEquals(serviceRouter.getThreadPoolExecutor().getCorePoolSize(), 1);
-        assertEquals(serviceRouter.getThreadPoolExecutor().getMaximumPoolSize(), Integer.MAX_VALUE);
-        assertEquals(serviceRouter.getThreadPoolExecutor().getKeepAliveTime(TimeUnit.SECONDS), 120);
+        assertEquals(serviceRouter.getThreadPoolExecutor().getCorePoolSize(), 200);
+        assertEquals(serviceRouter.getThreadPoolExecutor().getMaximumPoolSize(),500);
+        assertEquals(serviceRouter.getThreadPoolExecutor().getKeepAliveTime(TimeUnit.SECONDS),5 * 60);
     }
+
 //
 //    AnnotationServletServiceRouter serviceRouter;
 
@@ -66,42 +74,42 @@ public class RopNamespaceHandlerIT extends UnitilsTestNG {
      *
      * @param serviceRouter
      */
-    @Test
-    @SpringBeanByType
-    @SpringApplicationContext("com/rop/config/fullRopConfig.xml")
-    public void testFullConfig(AnnotationServletServiceRouter serviceRouter) {
-        assertNotNull(serviceRouter);
-        RopContext ropContext = serviceRouter.getRopContext();
-        assertNotNull(ropContext);
-        assertTrue(!ropContext.isSignEnable());
-        assertNotNull(serviceRouter.getApplicationContext());
-        assertEquals(serviceRouter.getExtErrorBasename(), "i18n/rop/sampleRopError");
-        assertEquals(serviceRouter.getInterceptors().size(), 1);
-        assertEquals(serviceRouter.getListeners().size(), 2);
-        assertNotNull(serviceRouter.getFormattingConversionService());
-        assertTrue(serviceRouter.getFormattingConversionService().canConvert(String.class, Telephone.class));
-
-        assertFalse(serviceRouter.isSignEnable());
-        DefaultSecurityManager validator = (DefaultSecurityManager) serviceRouter.getSecurityManager();
-        assertNotNull(validator);
-        assertTrue(validator.getAppSecretManager() instanceof SampleAppSecretManager);
-        assertTrue(validator.getServiceAccessController() instanceof SampleServiceAccessController);
-
-        assertNotNull(serviceRouter.getRopEventMulticaster());
-        assertNotNull(serviceRouter.getThreadPoolExecutor());
-        assertEquals(serviceRouter.getThreadPoolExecutor().getCorePoolSize(), 2);
-        assertEquals(serviceRouter.getThreadPoolExecutor().getMaximumPoolSize(), 100);
-        assertEquals(serviceRouter.getThreadPoolExecutor().getKeepAliveTime(TimeUnit.SECONDS), 200);
-        assertEquals(serviceRouter.getThreadPoolExecutor().getQueue().remainingCapacity(), 120);
-        assertEquals(serviceRouter.getServiceTimeoutSeconds(), 10);
-
-        assertEquals(SystemParameterNames.getAppKey(), "a1");
-        assertEquals(SystemParameterNames.getFormat(), "f1");
-        assertEquals(SystemParameterNames.getSessionId(), "s1");
-        assertEquals(SystemParameterNames.getSign(), "s2");
-        assertEquals(SystemParameterNames.getLocale(), "l1");
-        assertEquals(SystemParameterNames.getMethod(), "m1");
-        assertEquals(SystemParameterNames.getVersion(), "v1");
-    }
+//    @Test
+//    @SpringBeanByType
+//    @SpringApplicationContext("com/rop/config/fullRopConfig.xml")
+//    public void testFullConfig(AnnotationServletServiceRouter serviceRouter) {
+//        assertNotNull(serviceRouter);
+//        RopContext ropContext = serviceRouter.getRopContext();
+//        assertNotNull(ropContext);
+//        assertTrue(!ropContext.isSignEnable());
+//        assertNotNull(serviceRouter.getApplicationContext());
+//        assertEquals(serviceRouter.getExtErrorBasename(), "i18n/rop/sampleRopError");
+//        assertEquals(serviceRouter.getInterceptors().size(), 1);
+//        assertEquals(serviceRouter.getListeners().size(), 2);
+//        assertNotNull(serviceRouter.getFormattingConversionService());
+//        assertTrue(serviceRouter.getFormattingConversionService().canConvert(String.class, Telephone.class));
+//
+//        assertFalse(serviceRouter.isSignEnable());
+//        DefaultSecurityManager validator = (DefaultSecurityManager) serviceRouter.getSecurityManager();
+//        assertNotNull(validator);
+//        assertTrue(validator.getAppSecretManager() instanceof SampleAppSecretManager);
+//        assertTrue(validator.getServiceAccessController() instanceof SampleServiceAccessController);
+//
+//        assertNotNull(serviceRouter.getRopEventMulticaster());
+//        assertNotNull(serviceRouter.getThreadPoolExecutor());
+//        assertEquals(serviceRouter.getThreadPoolExecutor().getCorePoolSize(), 2);
+//        assertEquals(serviceRouter.getThreadPoolExecutor().getMaximumPoolSize(), 100);
+//        assertEquals(serviceRouter.getThreadPoolExecutor().getKeepAliveTime(TimeUnit.SECONDS), 200);
+//        assertEquals(serviceRouter.getThreadPoolExecutor().getQueue().remainingCapacity(), 120);
+//        assertEquals(serviceRouter.getServiceTimeoutSeconds(), 10);
+//
+//        assertEquals(SystemParameterNames.getAppKey(), "a1");
+//        assertEquals(SystemParameterNames.getFormat(), "f1");
+//        assertEquals(SystemParameterNames.getSessionId(), "s1");
+//        assertEquals(SystemParameterNames.getSign(), "s2");
+//        assertEquals(SystemParameterNames.getLocale(), "l1");
+//        assertEquals(SystemParameterNames.getMethod(), "m1");
+//        assertEquals(SystemParameterNames.getVersion(), "v1");
+//    }
 }
 
